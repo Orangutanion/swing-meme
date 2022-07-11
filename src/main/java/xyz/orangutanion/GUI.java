ad9cc66da7d6d56e6ef7cb23ab3fe481cb03e443
Orangutanion <keinkaihere@gmail.com>
2022-07-10 22:13:38 -0400

    made it work

--- src/main/java/xyz/orangutanion/GUI.java
+++ src/main/java/xyz/orangutanion/GUI.java
@@ -1,5 +1,7 @@
 package xyz.orangutanion;
 
+import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -11,43 +13,54 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class GUI extends JFrame {
 
 	// Needed to stop Eclipse from complaining (I'm not gonna serialize this though)
 	private static final long serialVersionUID = 4647480435550859897L;
+	
+	// Because we want image and text to have same size, we should divide size if too large for window
+	private final int sizeDenominator = 2;
 
 	GUI() {
 		super("Swing Meme");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(800, 600);
+		this.setSize(600, 800);
 		this.setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-		
-		try {
-			this.add(memeText(new File("src/assets/expand01.png"), "quack"));
-		}
-		catch(IOException e) {
-			System.out.println("Image not found");
-		}
 	}
 	
 	public void render() {
 		this.setVisible(true);
 	}
 	
-	private JComponent memeText(File image, String text) throws IOException {
+	public void addPanel(File image, String text) {
 		// Read image and turn into icon
-		BufferedImage picture = ImageIO.read(image);
-		JLabel memeIcon = new JLabel(new ImageIcon(picture));
+		BufferedImage picture;
+		try {
+			picture = ImageIO.read(image);
+		} catch (IOException e) {
+			System.out.println("File " + image.toString() + " not found.");
+			return;
+		}
+		Dimension itemSize = new Dimension
+				(picture.getWidth() / sizeDenominator, picture.getHeight() / sizeDenominator);
+		Image scaled = picture.getScaledInstance(itemSize.width, itemSize.height, Image.SCALE_DEFAULT);
 		
-		// Generate text
-		JLabel memeText = new JLabel(text);
+		JLabel memeIcon = new JLabel(new ImageIcon(scaled));
+		memeIcon.setPreferredSize(itemSize);
+		
+		// Generate text and format to have same size as image
+		JTextArea memeText = new JTextArea(text);
+		memeText.setPreferredSize(itemSize);
+		memeText.setLineWrap(true);
+		memeText.setEditable(false);
 		
 		// Add both to new panel
 		JComponent memePanel = new JPanel();
 		memePanel.add(memeIcon);
 		memePanel.add(memeText);
 		
-		return memePanel;
+		this.add(memePanel);
 	}
 }
